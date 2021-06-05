21e11fed7428bf0f21d7bb7bd5b97af000d8f14a
Keanu Ellwood <kellwood@jhb.dvt.co.za>
2021-06-05 21:02:24 +0200

    Boilerplate

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
+import { LandingComponent } from './pages/landing/landing.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // {
+  //   path: '',
+  //   pathMatch: 'full',
+  //   redirectTo: 'welcome'
+  // },
+  {
+    path: '',
+    component: LandingComponent
+  },
+  { 
+    path: "**", 
+    redirectTo: '' 
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
