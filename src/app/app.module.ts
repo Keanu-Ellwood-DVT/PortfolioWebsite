977dd79c638ba1dd73e857623674d5e243455253
Keanu Ellwood <kellwood@jhb.dvt.co.za>
2021-06-06 13:35:03 +0200

    Add main components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LandingComponent } from './pages/landing/landing.component';
 import { NavigationComponent } from './components/navigation/navigation.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { WelcomeComponent } from './components/welcome/welcome.component';
+import { AboutComponent } from './components/about/about.component';
+import { ProjectsComponent } from './components/projects/projects.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LandingComponent,
-    NavigationComponent
+    NavigationComponent,
+    ContactComponent,
+    WelcomeComponent,
+    AboutComponent,
+    ProjectsComponent
   ],
   imports: [
     BrowserModule,
